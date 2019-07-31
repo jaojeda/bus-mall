f900c46ee317871293faf88e9728b3a37fed6d78
jaojeda <j.a.ojeda@live.com>
2019-07-31 10:29:43 -0700

    worked on getting random objects to generate

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import store from './data/store.js';
+import ProductList from './product-set.js';
 
 const instructions = document.getElementById('instructions-box');
 const startButton = document.getElementById('start-button');
@@ -5,9 +7,23 @@ const quizSection = document.getElementById('quiz-section');
 
 startButton.addEventListener('click', startTest);
 
-const product =     
+const products = store.getProducts();
+const masterProductsList = new ProductList(products);
+let reloadList = [];
+
+const productOne = masterProductsList.getRandomProduct();
+const displayList = new ProductList(products);
+displayList.removeById(productOne.id);
+const productTwo = displayList.getRandomProduct();
+displayList.removeById(productTwo.id);
+const productThree = displayList.getRandomProduct();
+displayList.removeById(productThree.id);
+reloadList.push(displayList.list);
+
+console.log(productOne);
+
 
 function startTest() {
     instructions.classList.add('hidden');
     quizSection.classList.remove('hidden');
-};
+}
