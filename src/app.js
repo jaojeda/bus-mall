856e4a67e0eec348197b97d2ac12ba25ae8c3095
jaojeda <j.a.ojeda@live.com>
2019-07-31 12:49:07 -0700

    added basis for reload function, corrected css issue with footer

--- src/app.js
+++ src/app.js
@@ -6,23 +6,35 @@ const instructions = document.getElementById('instructions-box');
 const startButton = document.getElementById('start-button');
 const quizSection = document.getElementById('quiz-section');
 const buttonContainer = document.getElementById('button-container');
+// const resultSection = document.getElementById('result-table');
 
 startButton.addEventListener('click', startTest);
 
 const products = store.getProducts();
 const masterProductsList = new ProductList(products);
+const standByList = [];
 
 generateFirstChoice();
+
+const displayList = new ProductList(products);
+for(let i = 0; 0 < standByList.length; i++) {
+    displayList.removeById(standByList[i].id);
+}
+
 
 
 function generateFirstChoice() {
     const productOne = masterProductsList.getRandomProduct();
+    standByList.push(productOne);
     const displayList = new ProductList(products);
     displayList.removeById(productOne.id);
     const productTwo = displayList.getRandomProduct();
+    standByList.push(productTwo);
     displayList.removeById(productTwo.id);
     const productThree = displayList.getRandomProduct();
-    displayList.removeById(productThree.id);   
+    standByList.push(productThree);
+    displayList.removeById(productThree.id);
+
 
     buttonContainer.appendChild(renderChoice(productOne));
     buttonContainer.appendChild(renderChoice(productTwo));
