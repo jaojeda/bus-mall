27954560a141a2f9a3560ad873f19cd566ae65d4
jaojeda <j.a.ojeda@live.com>
2019-07-31 16:04:32 -0700

    got new buttons to generate on click but caused repeat bug

--- src/app.js
+++ src/app.js
@@ -6,22 +6,49 @@ const instructions = document.getElementById('instructions-box');
 const startButton = document.getElementById('start-button');
 const quizSection = document.getElementById('quiz-section');
 const buttonContainer = document.getElementById('button-container');
+const chosenButton = document.getElementsByClassName('product-choice');
 // const resultSection = document.getElementById('result-table');
 
 startButton.addEventListener('click', startTest);
 
 const products = store.getProducts();
 const masterProductsList = new ProductList(products);
-const standByList = [];
+let standByList = [];
 
 generateFirstChoice();
 
-const displayList = new ProductList(products);
-for(let i = 0; 0 < standByList.length; i++) {
-    displayList.removeById(standByList[i].id);
+chosenButton[0].addEventListener('click', reloadChoices);
+chosenButton[1].addEventListener('click', reloadChoices);
+chosenButton[2].addEventListener('click', reloadChoices);
+
+
+function reloadChoices() {
+    const displayList = new ProductList(products);
+    for(let i = 0; i < standByList.length; i++) {
+        displayList.removeById(standByList[i].id);
+    }
+
+    standByList = [];
+    
+    const productOne = displayList.getRandomProduct();
+    standByList.push(productOne);
+    const productTwo = displayList.getRandomProduct();
+    standByList.push(productTwo);
+    const productThree = displayList.getRandomProduct();
+    standByList.push(productThree);
+
+    while(buttonContainer.firstChild) {
+        buttonContainer.removeChild(buttonContainer.firstChild);
+    }
+
+    buttonContainer.appendChild(renderChoice(productOne));
+    buttonContainer.appendChild(renderChoice(productTwo));
+    buttonContainer.appendChild(renderChoice(productThree));
+
+    chosenButton[0].addEventListener('click', reloadChoices);
+    chosenButton[1].addEventListener('click', reloadChoices);
+    chosenButton[2].addEventListener('click', reloadChoices);
 }
-
-
 
 function generateFirstChoice() {
     const productOne = masterProductsList.getRandomProduct();
